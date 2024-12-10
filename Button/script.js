c6d30c85f192d4805dd62a1dfc743a1e8bf6158e
Ethan Podhorez <p.ethanpod@gmail.com>
2024-12-09 20:08:46 -0500

    Delete Button directory

--- Button/script.js
+++
@@ -1,8 +0,0 @@
-const button = document.getElementById("button");
-const num = document.getElementById("num");
-let count = 0;
-
-button.addEventListener('click', function() {
-    count += 1;
-    num.innerHTML = count + 1;
-})
