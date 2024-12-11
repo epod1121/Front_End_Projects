46e60e290286667718fb18887f047eff5fa03280
Ethan Podhorez <p.ethanpod@gmail.com>
2024-12-10 20:47:10 -0500

    Add files via upload

---
+++ Serious_Projects/App.jsx
@@ -0,0 +1,14 @@
+import Header from './Header/Header.jsx'
+import Display from './Display/Display.jsx';
+import './Styles.css'
+
+function App() {
+  return(
+    <>
+    <Header />
+    <Display />
+    </>
+  );
+}
+
+export default App
---
+++ Serious_Projects/Header/Header.jsx
@@ -0,0 +1,9 @@
+import styles from './Header.module.css'
+
+function Header() {
+    return(
+        <h1 className={styles.Header}>Car Payment Calculator 🚗</h1>
+    );
+}
+
+export default Header
