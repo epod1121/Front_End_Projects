94df5cc961c456362717145b3568499c8461ec95
Ethan Podhorez <p.ethanpod@gmail.com>
2024-12-10 20:59:26 -0500

    Delete Front_End_Projects/Not_Serious_Projects directory

--- Front_End_Projects/Not_Serious_Projects/MouseMovement/script.js
+++
@@ -1,8 +0,0 @@
-const x_coord_text = document.getElementById("x_text");
-const y_coord_text = document.getElementById("y_text");
-
-document.addEventListener("mousemove", (event) => {
-    x_coord_text.innerHTML = event.clientX;
-    y_coord_text.innerHTML = event.clientY;
-
-});
